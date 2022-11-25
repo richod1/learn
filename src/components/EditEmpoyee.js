e0b8f20beb9ac950884535c5c446d232f84ac85b
richod1 <frimpsup@gmail.com>
2022-11-25 03:09:01 -0800

    fixed:modal button

--- src/components/EditEmpoyee.js
+++ src/components/EditEmpoyee.js
@@ -23,7 +23,7 @@ function EditEmployee() {
           <Modal.Title>Update Employee</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-        <form className="w-full max-w-sm">
+        <form id="editmodal" className="w-full max-w-sm">
      <div className="md:flex md:items-center mb-6">
     <div className="md:w-1/3">
       <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="name">
@@ -43,22 +43,17 @@ function EditEmployee() {
     <div className="md:w-2/3">
       <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="role" type="text" value="web dev"/>
     </div>
-  </div><div className="md:flex md:items-center">
-    <div className="md:w-1/3"></div>
-    <div className="md:w-2/3">
-      <button className="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="button">
-        Sign Up
-      </button>
-    </div>
   </div>
 </form>
   
         </Modal.Body>
         <Modal.Footer>
+            
           <Button variant="secondary" onClick={handleClose}>
             Close
           </Button>
-          <Button variant="primary">Update</Button>
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" form="editmodal">Update</button>
+          
         </Modal.Footer>
       </Modal>
     </>
